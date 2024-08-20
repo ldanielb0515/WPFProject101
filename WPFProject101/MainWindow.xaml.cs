9747b424c9299844cdccb55a45e00c7b5484de9a
Lee Balmonte <Lee Balmonte@LAPTOP-UDFMGI58>
2024-08-20 13:03:36 +0700

    Add project files.

---
+++ WPFProject101/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFProject101
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetCalculate();
+        }
+
+        private void SetCalculate()
+        {
+            List<string> randLetter = new List<string>()
+            {
+                "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock txtblock in maingrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(randLetter.Count);
+                string letter = randLetter[index];
+                txtblock.Text = letter;
+                randLetter.RemoveAt(index);
+            }
+        }
+    }
+}
